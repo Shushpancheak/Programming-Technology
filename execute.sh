aff0016825c79aa88a1a38da662dd537aedda81e
Shushpancheak <Makcieml@gmail.com>
2019-04-07 23:06:53 +0300

    first commit

---
+++ execute.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "AbstractFactory"
+./PatternsCollection/bin-arm/AbstractFactory
+echo "Builder"
+./PatternsCollection/bin-arm/Builder
+echo "Facade"
+./PatternsCollection/bin-arm/Facade
+echo "Prototype"
+./PatternsCollection/bin-arm/Prototype
