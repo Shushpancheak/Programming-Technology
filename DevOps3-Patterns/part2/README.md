94a1de3520e8afbc4e8cf23631cc52471ca305be
Shushpancheak <Makcieml@gmail.com>
2019-04-08 07:18:13 +0300

    second task

---
+++ DevOps3-Patterns/part2/README.md
@@ -0,0 +1,17 @@
+# Part 2: Notifier.
+
+## Steps to compile:
+Make sure you have installed necessary packages and their dependencies: `curl, OpenSSL, vmime, sendmail*, mailutils, Boost, GnuTLS`
+Maybe you'll need others, then just download them when an error occur.
+
+```bash
+chmod +x build-and-execute.sh
+./build-and-execute.sh
+```
+
+## Steps to accomplish the task:
+* Firstly, I needed to use VPN to create a GMail account.
+* Modified main CMakeLIsts.txt by commenting every project but Decorator.
+* Created config.ini and filled it with data.
+* Modified vmime CMakeLIsts.txt in way that it wouldn't throw error if there's no VMIME_SENDMAIL_PATH by adding FORCE in set (of that) variable.
+* Had to install VPN on my machine in order to get ./Decorator work properly.
---
+++ DevOps3-Patterns/part2/build-and-execute.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "...Cloning main repo..."
+
+git clone https://github.com/akhtyamovpavel/PatternsCollection.git
+cd PatternsCollection
+
+echo "...Adding config.ini..."
+rm Decorator/cpp-source/configs/config.ini
+cp ../config.ini Decorator/cpp-source/configs/config.ini
+
+echo "...Modifying CMakeLists to have only Decorator project..."
+
+rm CMakeLists.txt
+cp ../CMakeLists.txt CMakeLists.txt
+
+echo "...Building project..."
+
+mkdir build
+cd build
+
+cmake ..
+
+echo "...Changing SendMail library because of bug..."
+
+rm build/Decorator/cpp-source/vmime_project-prefix/src/vmime_project/CMakeLists.txt
+cp ../VMIME_CMakeLists.txt build/Decorator/cpp-source/vmime_project-prefix/src/vmime_project/CMakeLists.txt
+
+echo "...Compiling..."
+
+make
+
+echo "...Succes; executing..."
+
+cd ../bin
+./Decorator
