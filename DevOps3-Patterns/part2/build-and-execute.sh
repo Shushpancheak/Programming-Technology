77655344edadf158fa8c5b5919a5f85bc7d10c9d
Shushpancheak <Makcieml@gmail.com>
2019-04-10 00:16:10 +0300

    EmailNotifier smtp fix

--- DevOps3-Patterns/part2/build-and-execute.sh
+++ DevOps3-Patterns/part2/build-and-execute.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "...Cloning main repo..."
-
 git clone https://github.com/akhtyamovpavel/PatternsCollection.git
 cd PatternsCollection
 
@@ -9,28 +8,27 @@ echo "...Adding config.ini..."
 rm Decorator/cpp-source/configs/config.ini
 cp ../config.ini Decorator/cpp-source/configs/config.ini
 
-echo "...Modifying CMakeLists to have only Decorator project..."
+echo "...Modifying EmailNotifier.cpp..."
+rm Decorator/cpp-source/notifier/EmailNotifier.cpp
+cp ../EmailNotifier.cpp Decorator/cpp-source/notifiers/EmailNotifier.cpp
 
+echo "...Modifying CMakeLists to have only Decorator project..."
 rm CMakeLists.txt
 cp ../CMakeLists.txt CMakeLists.txt
 
 echo "...Building project..."
-
 mkdir build
 cd build
 
 cmake ..
 
 echo "...Changing SendMail library because of bug..."
-
 rm Decorator/cpp-source/vmime_project-prefix/src/vmime_project/CMakeLists.txt
 cp ../../VMIME_CMakeLists.txt Decorator/cpp-source/vmime_project-prefix/src/vmime_project/CMakeLists.txt
 
 echo "...Compiling..."
-
 make
 
 echo "...Succes; executing..."
-
 cd ../bin
 ./Decorator
